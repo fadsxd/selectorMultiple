e09daa77a5c529280142e3b543ae0be667083e4d
micky <mickyfads5@gmail.com>
2023-12-30 13:11:52 -0400

    selectorApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountiresModule } from './countries/countires.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'selector',
+  loadChildren:  () => import('./countries/countires.module').then( m  => CountiresModule) },
+  {
+    path:'**',
+    redirectTo: 'selector',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
